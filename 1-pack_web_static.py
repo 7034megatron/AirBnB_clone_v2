a24d81087694e2689978e304e33257d429286dc6
7034megatron <emmanuelmabhena0@gmail.com>
2024-04-04 10:07:36 -0700

    corrected 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,34 +1,20 @@
 #!/usr/bin/python3
-"""
-Fabric script that generates a .tgz archive from the contents of the web_static folder
-"""
-from fabric.api import local
+""" This module contains the function do_pack that generates a .tgz archive
+  from the contents of the web_static folder (fabric script) """
+
+
+from fabric.api import *
 from datetime import datetime
 
 
 def do_pack():
-    """
-    Generates a .tgz archive from the contents of the web_static folder
-
-    Returns:
-        str: Path to the archive if it has been correctly generated, None otherwise
-    """
-    try:
-        # Create versions directory if it doesn't exist
-        local("mkdir -p versions")
-
-        # Generate timestamp for archive name
-        now = datetime.now()
-        timestamp = now.strftime("%Y%m%d%H%M%S")
-
-        # Create the .tgz archive
-        archive_path = "versions/web_static_{}.tgz".format(timestamp)
-        local("tar -cvzf {} web_static".format(archive_path))
-
-        return archive_path
-    except Exception as e:
+    """ Fabric script that generates a .tgz archive from the contents of the...
+    ...web_static folder """
+    local("sudo mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = "versions/web_static_{}.tgz".format(date)
+    result = local("sudo tar -cvzf {} web_static".format(filename))
+    if result.succeeded:
+        return filename
+    else:
         return None
-
-
-if __name__ == "__main__":
-    do_pack()
